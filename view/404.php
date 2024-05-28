2712191a542714c36fa5e3b3a2c337fc7231f2ad
evelynct <evelyncelestino23@gmail.com>
2024-05-28 20:10:11 -0300

    E 1.0 "Style=error"

--- view/404.php
+++ view/404.php
@@ -2,6 +2,6 @@
     <div class="container-erro">
         <h1>ERRO 404 - Página não encontrada</h1>
         <img src="./assets/img/MikeWazolski.png" alt="Mike Wazowski">
-    <div class="mike"></div>
+    <div class="mike"></div> 
     </div>
 </section>
