086bb80ed259e603386cb084042d4229879fc9c7
guikain <guilhermesentone@gmail.com>
2024-06-09 18:13:25 -0300

    correção na home

--- view/home.php
+++ view/home.php
@@ -5,6 +5,6 @@
         <p>Registre sua conta para começar a colaborar ou faça login para acessar projetos existentes.</p>
         <p>Acompanhe sua participação através de métricas detalhadas, incluindo horas dedicadas, descrições enviadas e status geral do projeto.</p>
         <p>Prepare-se para uma experiência de colaboração simplificada e eficiente!</p>
-        <p><a href="cadastro.php">Crie sua conta</a> ou <a href="login.php">faça login</a> para começar.</p>
+        <p><a href="?p=cadusr">Crie sua conta</a> ou <a href="?p=login">faça login</a> para começar.</p>
     </div>
 </section>
